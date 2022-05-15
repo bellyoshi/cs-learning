5c4861b3d30462c6c26797f69918d60d6d05139e
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-05-15 10:03:55 +0900

    refact

--- Refactoring/chapter-1/VideoRental/Movie.cs
+++ Refactoring/chapter-1/VideoRental/Movie.cs
@@ -23,5 +23,27 @@ public class Movie
     public string getTitle() => _title;
     
     public int getPriceCode() => _priceCode;
-    
+
+    internal double getCharge(int daysRented)
+    {
+        double result = 0;
+        switch (getPriceCode())
+        {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
 }
