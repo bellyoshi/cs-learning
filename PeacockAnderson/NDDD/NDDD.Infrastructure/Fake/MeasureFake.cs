acc352f226e27c08e45f554c0b4ee33ccf88f331
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-08-09 08:23:20 +0900

    bind

---
+++ PeacockAnderson/NDDD/NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -0,0 +1,12 @@
+﻿using NDDD.Domain.Entities;
+using NDDD.Domain.Repositories;
+
+namespace NDDD.Infrastructure.Fake;
+
+public class MeasureFake : IMeasureRepository
+{
+    MeasureEntity IMeasureRepository.GetLatest()
+    {
+        return new MeasureEntity(10, Convert.ToDateTime("2020/12/12 12:34:56"), 123.341f);
+    }
+}
--- PeacockAnderson/NDDD/NDDD.WinForm/Program.cs
+++ PeacockAnderson/NDDD/NDDD.WinForm/Program.cs
@@ -11,7 +11,7 @@ namespace NDDD.WinForm
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Views.LatestView());
         }
     }
 }
