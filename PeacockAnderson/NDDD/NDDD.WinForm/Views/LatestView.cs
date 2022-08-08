b2c02b5942f92db19826441949f5b1edaf8feb1a
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-08-09 08:23:20 +0900

    bind

---
+++ PeacockAnderson/NDDD/NDDD.WinForm/Views/LatestView.cs
@@ -0,0 +1,26 @@
+﻿using NDDD.WinForm.ViewModels;
+
+namespace NDDD.WinForm.Views
+{
+    public partial class LatestView : Form
+    {
+        private LatestViewModel _viewModel = new LatestViewModel();
+        public LatestView()
+        {
+            InitializeComponent();
+
+            AddDataBingings();
+        }
+
+        private void AddDataBingings()
+        {
+            this.AreaIDTextBox.DataBindings.Add("Text", _viewModel, "AreaIdText");
+            this.MeasureDateTextBox.DataBindings.Add("Text", _viewModel, "MeasureDateText");
+            this.MeasureValueTextbox.DataBindings.Add("Text", _viewModel, "MeasureValueText");
+        }
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
