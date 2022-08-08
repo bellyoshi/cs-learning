ac43f2fee2f288e8a61962f5efbdb5decff0e459
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-08-08 15:58:04 +0900

    最初のテスト

---
+++ PeacockAnderson/NDDD/NDDD.WinForm/ViewModels/LatestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.WinForm.ViewModels
+{
+    public class LatestViewModel
+    {
+        public string AreaIdText { get; set; }
+        public string MeasureDateText { get; set; }
+        public string MeasureValueText { get; set; }
+
+        public void Search()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PeacockAnderson/NDDD/NDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
@@ -0,0 +1,39 @@
+﻿using NDDD.WinForm.ViewModels;
+
+
+namespace NDDTest.Tests.ViewModelTests;
+
+
+[TestClass]
+public class LatestViewModelTest
+{
+
+    [TestMethod]
+    public void シナリオ()
+    {
+        var vm = new LatestViewModel();
+        vm.Search();
+        vm.AreaIdText.Is("001");
+        vm.MeasureDateText.Is("2012/12/12 12:34:56");
+        vm.MeasureValueText.Is("12.34℃");
+
+        //// モックのデータを作成
+        //var entity = new MeasureEntity(
+        //    1,
+        //    Convert.ToDateTime("2020/05/26 22:00:00"),
+        //    12.34f);
+
+        //// Moqセット
+        //var measureMock = new Mock<IMeasureRepository>();
+        //measureMock.Setup(x => x.GetLatest()).Returns(entity);
+
+        //var vm = new LatestViewModel(measureMock.Object);
+        //// エリアID
+        //// 計測日時
+        //// 計測値
+
+
+
+
+    }
+}
