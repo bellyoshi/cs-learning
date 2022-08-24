82e7fe7740db86707a7287dbc4f6bbb1e301d090
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-08-24 17:16:32 +0900

    MVVM2

---
+++ MVVM2/MVVM2/Binder.cs
@@ -0,0 +1,42 @@
+﻿using Reactive.Bindings;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM2;
+
+public static class Binder
+{
+    public static void Bind<T, U>(Expression<Func<T>> item1, Expression<Func<U>> item2)
+    {
+
+        var tuple1 = ResolveLambda(item1);
+        var tuple2 = ResolveLambda(item2);
+        var control = tuple1.Item1 as Control;
+        if (control == null) throw new ArgumentException();
+        control.DataBindings.Add(new Binding(tuple1.Item2, tuple2.Item1, tuple2.Item2));
+    }
+    static Tuple<object, string> ResolveLambda<V>(Expression<Func<V>> expression)
+    {
+        var lambda = expression as LambdaExpression;
+        if (lambda == null) throw new ArgumentException();
+        var property = lambda.Body as MemberExpression;
+        if (property == null) throw new ArgumentException();
+        var members = new List<MemberInfo>();
+        var parent = property.Expression;
+        return new Tuple<object, string>(Expression.Lambda(parent).Compile().DynamicInvoke(), property.Member.Name);
+    }
+    public static void Bind<T>(this Label label, Expression<Func<T>> expression)
+    {
+        Bind(() => label.Text, expression);
+    }
+
+    public static void Bind(this Button button, ReactiveCommand command)
+    {
+        command.CanExecuteChanged += (sender, args) => button.Enabled = command.CanExecute();
+        button.Enabled = command.CanExecute();
+    //    button.Visible = command.Visibility;
+        button.Click += (sender, args) => command.Execute();
+    }
+}
---
+++ MVVM2/MVVM2/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+
+namespace MVVM2;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    public ReactiveProperty<int> Counter { get; } = new ReactiveProperty<int>();
+
+    public ReactiveCommand UpCommand { get; private set; }
+    public ReactiveCommand DownCommand { get; private set; }
+
+    public ViewModel()
+    {
+        UpCommand = Counter.Select(_ => Counter.Value < 10).ToReactiveCommand();
+        UpCommand.Subscribe(() => Counter.Value++);
+        DownCommand = Counter.Select(_ => Counter.Value > 0).ToReactiveCommand();
+        DownCommand.Subscribe(() => Counter.Value--);
+    }
+}
