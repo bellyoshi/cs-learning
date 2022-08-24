2a44268e2d7761b6ba8094123958962bca0a3fe3
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-08-24 21:00:01 +0900

    Blog

---
+++ PeacockAnderson/NDDD/NDDD.Infrastructure/EF/MeasureEF.cs
@@ -0,0 +1,20 @@
+﻿using NDDD.Domain.Repositories;
+using NDDD.Domain.Entities;
+namespace NDDD.Infrastructure.EF;
+
+public class MeasureEF : IBlogRepository
+{
+    private readonly NdddDbContext _context;
+
+
+    public MeasureEF(NdddDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public Blog? GetLatest()
+    {
+        return _context.Blogs.OrderByDescending(m => m.BlogId).FirstOrDefault();
+    }
+    
+}
