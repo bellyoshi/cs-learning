56881fda85329d98aca5fb13926923d08201fad5
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-09-06 09:58:40 +0900

    Exception

--- PeacockAnderson/NDDD/NDDD.WinForm/Views/BaseForm.cs
+++ PeacockAnderson/NDDD/NDDD.WinForm/Views/BaseForm.cs
@@ -6,28 +6,37 @@ using System.Drawing;
 using System.Linq;
 using NDDD.Domain;
 
-namespace NDDD.WinForm.Views
+namespace NDDD.WinForm.Views;
+
+public partial class BaseForm : Form
 {
-    public partial class BaseForm : Form
+    private static ILog _logger = AppLog.GetLogger();
+
+    public BaseForm()
     {
-        public BaseForm()
-        {
-            InitializeComponent();
-            DebugModeDisp();
-            UserIdDisp();
-        }
+        InitializeComponent();
+        DebugModeDisp();
+        UserIdDisp();
+    }
 
-        private void UserIdDisp()
-        {
-            UserIdLabel.Text = CurrentUser.LoginId;
-        }
+    private void UserIdDisp()
+    {
+        UserIdLabel.Text = CurrentUser.LoginId;
+    }
 
-        private void DebugModeDisp()
-        {
-            DebugStatusLabel.Visible = false;
+    private void DebugModeDisp()
+    {
+        DebugStatusLabel.Visible = false;
 #if DEBUG
-            DebugStatusLabel.Visible = true;
+        DebugStatusLabel.Visible = true;
 #endif
-        }
+    }
+    private void BaseForm_Load(object sender, EventArgs e)
+    {
+        _logger.Info("open:" + this.Name);
+    }
+    private void BaseForm_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        _logger.Info("close:" + this.Name);
     }
 }
---
+++ PeacockAnderson/NDDD/NDDD.WinForm/Views/ExceptionClas.cs
@@ -0,0 +1,40 @@
+﻿using NDDD.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.WinForm.Views
+{
+    internal class ExceptionClas
+    {
+        private static ILog _logger = AppLog.GetLogger();
+
+        protected void ExceptionProc(Exception ex)
+        {
+
+            _logger.Error(ex);
+            
+            MessageBoxIcon icon = MessageBoxIcon.Error;
+            string caption = "エラー";
+
+            if (ex is ExceptionBase exceptionBase)
+            {
+
+                if (exceptionBase.Kind == ExceptionBase.ExceptionKind.Info)
+                {
+                    icon = MessageBoxIcon.Information;
+                    caption = "情報";
+                }
+                else if (exceptionBase.Kind == ExceptionBase.ExceptionKind.Warning)
+                {
+                    icon = MessageBoxIcon.Warning;
+                    caption = "警告";
+                }
+            }
+
+            MessageBox.Show(ex.Message, caption, MessageBoxButtons.OK, icon);
+        }
+    }
+}
