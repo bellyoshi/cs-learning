ac56c28e18e12516e8ec100e25f4de500680b28a
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2022-10-23 20:56:56 +0900

    g

---
+++ MultiCore/C_sharp_MultiCore_Part1/Part01Chapter02/gluttony/gluttony/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace gluttony
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(51, 48);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(110, 35);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(188, 48);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(110, 35);
+            this.StopButton.TabIndex = 0;
+            this.StopButton.Text = "STOP";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button StartButton;
+        private Button StopButton;
+    }
+}
