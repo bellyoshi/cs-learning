621ba1e17e5144beba9331f676c29e58da7e86ed
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-09-03 21:19:49 +0900

    handle削除

--- uwp/viewerproto/ViewerPage.xaml.cs
+++ uwp/viewerproto/ViewerPage.xaml.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Data.Pdf;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
 using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
@@ -30,13 +20,13 @@ namespace viewerproto
         public ViewerPage()
         {
             this.InitializeComponent();
-            Handle = this;
+
         }
         private PdfDocument doc;
         private PdfPage page;
         private uint pageIndex;
 
-        public static ViewerPage Handle { get; private set; }
+
 
         private async void MyButton_Click(object sender, RoutedEventArgs e)
         {
