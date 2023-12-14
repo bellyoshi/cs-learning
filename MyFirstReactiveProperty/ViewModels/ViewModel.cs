f465756e5739433663de98a73f75bf57faa298f5
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-12-14 21:50:34 +0900

    #

---
+++ MyFirstReactiveProperty/ViewModels/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+
+namespace MyFirstReactiveProperty.ViewModels
+{
+    internal class ViewModel
+    {
+        public ReactiveProperty<string> InputValue { get; }
+        public ReactiveProperty<string?> OutputValue { get; }
+
+        public ViewModel()
+        {
+            InputValue = new ReactiveProperty<string>("");
+            OutputValue = InputValue.Select(x => x.ToUpper()).ToReactiveProperty();
+        }
+    }
+}
