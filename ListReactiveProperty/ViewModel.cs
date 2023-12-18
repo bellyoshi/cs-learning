e0f8decc77a856ba3c79fcc8396ed5b737591f98
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-12-18 21:26:33 +0900

    Converter

---
+++ ListReactiveProperty/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reactive.Bindings;
+using System.Reactive.Linq;
+using System.Configuration;
+
+namespace ListReactiveProperty
+{
+    internal class ViewModel
+    {
+        public ObservableCollection<string> CheckInLog { get; } = [];
+        public ReactiveCommand<string> CheckInCommand { get; } = new();
+        public ReadOnlyReactiveProperty<string?> FileName { get; }
+
+        public ViewModel()
+        {
+            this.FileName = CheckInCommand.ToReadOnlyReactiveProperty();
+            CheckInCommand.Subscribe(name =>
+            {
+                if (string.IsNullOrEmpty(name)) return;
+                CheckInLog.Add(name);
+            });
+        }
+
+
+
+    }
+}
