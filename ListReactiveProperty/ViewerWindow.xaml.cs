8e020d2d66e47d19b6fd45243da7b704adc3135e
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2023-12-27 17:09:28 +0900

    コメント修正

--- ListReactiveProperty/ViewerWindow.xaml.cs
+++ ListReactiveProperty/ViewerWindow.xaml.cs
@@ -10,24 +10,13 @@ namespace ListReactiveProperty
     {
         public ViewerWindow(ReactiveProperty<System.Windows.Media.Imaging.BitmapSource> ImageSource)
         {
+            //initializeComponentの前にDataContextを設定しなければWindowのサイズが変わらない
             DataContext ??= new ViewerViewModel(ImageSource);
             InitializeComponent();
 
-            //PositionWindowOnSecondaryMonitor();
+
         }
 
-        //private void PositionWindowOnSecondaryMonitor()
-        //{
-        //    var monitors = MonitorInfo.GetMonitors();
-        //    var secondaryMonitor = monitors[0]; 
-        //    if (monitors.Count > 1)
-        //    {
-        //        secondaryMonitor = monitors[1]; // 例として2番目のモニターを選択
 
-        //    }
-        //    this.Left = secondaryMonitor.WorkArea.Left;
-        //    this.Top = secondaryMonitor.WorkArea.Top;
-
-        //}
     }
 }
