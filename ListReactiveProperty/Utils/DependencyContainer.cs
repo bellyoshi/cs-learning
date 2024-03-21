d41c3a1c4b77881cc49efa6b503acbce2f74feda
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2024-03-21 22:51:43 +0900

    #

---
+++ ListReactiveProperty/Utils/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListReactiveProperty.Utils
+{
+    public class DependencyContainer
+    {
+        private readonly Dictionary<Type, object> instances = new Dictionary<Type, object>();
+
+        public void RegisterInstance<T>(T instance)
+        {
+            instances[typeof(T)] = instance;
+        }
+
+        public T GetInstance<T>()
+        {
+            if (instances.TryGetValue(typeof(T), out var instance))
+            {
+                return (T)instance;
+            }
+            throw new InvalidOperationException($"No instance registered for type {typeof(T)}");
+        }
+    }
+}
