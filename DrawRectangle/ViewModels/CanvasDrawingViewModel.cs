66df939220225134ae9ee930c695926cb90ff66e
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2024-03-24 15:32:17 +0900

    #

---
+++ DrawRectangle/ViewModels/CanvasDrawingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System.Windows;
+
+namespace DrawRectangle.ViewModels
+{
+    public class CanvasDrawingViewModel
+    {
+        public ReactivePropertySlim<string> Text { get; } = new ReactivePropertySlim<string>("クリックされていないな...");
+
+        public ReactiveCommand<Point> ClickLeftCommand { get; } = new ReactiveCommand<Point>();
+
+        public CanvasDrawingViewModel()
+        {
+            ClickLeftCommand.WithSubscribe(p => OnClickLeft(p));
+        }
+
+        private void OnClickLeft(Point p)
+        {
+
+            Text.Value = $"左クリックされたよ！ X: {p.X} Y: {p.Y}";
+        }
+    }
+}
