51041f24568c0a6386c88b48c95b13dfbbcdfd2d
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2024-03-26 21:36:32 +0900

    a

---
+++ OpenWindowSample/Behaviors/DependencyObjectOpenWindowBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenWindowSample.Behaviors
+{
+    internal class DependencyObjectOpenWindowBehavior : TriggerAction<DependencyObject>
+    {
+        protected override void Invoke(object parameter)
+        {
+            // 新しいWindowを作成・表示
+            var newWindow = new MainWindow();
+            newWindow.Show();
+        }
+    }
+}
