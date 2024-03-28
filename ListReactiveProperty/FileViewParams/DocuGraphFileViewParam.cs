0dbde6a27dc6ec2ea7eac997d904d190f499b833
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2024-03-29 05:58:01 +0900

    #

---
+++ ListReactiveProperty/FileViewParams/DocuGraphFileViewParam.cs
@@ -0,0 +1,32 @@
+﻿using ListReactiveProperty.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ListReactiveProperty.FileViewParams
+{
+    abstract internal class DocuGraphFileViewParam(string filename) : FileViewParam(filename), ImageSetter
+    {
+
+        private IDisplay? _display;
+        public void SetDisplay(IDisplay display)
+        {
+            _display = display;
+            ExecuteDisplay();
+        }
+
+        public void ExecuteDisplay()
+        {
+            if (_display == null) return;
+            var image = GetImageFromFile();
+            _display.SetImageSource(image);
+        }
+
+        public abstract BitmapSource GetImageFromFile();
+        
+    }
+}
