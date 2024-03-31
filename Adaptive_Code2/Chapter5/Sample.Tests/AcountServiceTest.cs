0003412d37cadf9f7b1c1dcba691b866554908c1
渡邉喜光 <bellmonzo.yoshi@hotmail.co.jp>
2024-03-31 14:38:50 +0900

    AccountService

---
+++ Adaptive_Code2/Chapter5/Sample.Tests/AcountServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Tests
+{
+    [TestClass]
+    public class AcountServiceTest
+    {
+        [TestMethod]
+        public void AddingTransactionToAccountDelegatesToAccountInstance()
+        {
+            // Arrange
+            var account = new Account();
+            var fakeAccountRepository = new FakeAccountRepository(account);
+            var sut = new AccountService(fakeAccountRepository);
+
+            //Act
+            sut.AddTransactionToAccount("Trading Account", 200m);
+
+            //Assert
+            Assert.AreEqual(200m, account.Balance);
+        }
+    }
+}
---
+++ Adaptive_Code2/Chapter5/Sample.Tests/FakeAccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Tests
+{
+    internal class FakeAccountRepository : IAccountRepository
+    {
+        private Account _account;
+        public FakeAccountRepository(Account account)
+        {
+            _account = account;
+        }
+
+        public Account GetByName(string accountName)
+        {
+            return _account;
+        }
+    }
+}
